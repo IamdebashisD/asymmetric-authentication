ea0ba0e620eab9643d8c2511ae0191a3da94fd98
IamdebashisD <adebashisdas626@gmail.com>
2026-07-03 02:32:25 +0530

    feat: create user model schema with validation for name, email, and password

---
+++ auth-server/src/models/user.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose"
+
+
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            minlength: 3,
+            maxlength: 50,
+            required: [true, "Name is required"],
+        },
+        email: {
+            type: String,
+            trim: true,
+            required: [true, "Email is required"],
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minlength: 8,
+            select: false
+        }        
+    }, 
+    { timestamps: true }
+)
+
+export default mongoose.model('User', userSchema)
