cdc0178aab7a63da625f8b0b9a9dc23259594fc2
IamdebashisD <adebashisdas626@gmail.com>
2026-07-04 00:19:58 +0530

    feat: establish database connection in server initialization

--- auth-server/src/server.js
+++ auth-server/src/server.js
@@ -1,12 +1,15 @@
 import 'dotenv/config.js'
 import { createServer } from 'node:http'
 import { createExpressApplication } from './app.js'
+import { connectDb } from './config/database.js'
 
 const PORT = process.env.PORT || 8080
 
 async function main() {
     try {
         const server = createServer(createExpressApplication())
+        
+        await connectDb()
 
         server.listen(PORT, () => {
             console.log(`Express server is running on post ${PORT}`)
