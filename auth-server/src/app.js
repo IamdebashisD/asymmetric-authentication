02404559edb3aa98f25a52d319c2e846350f8fc1
IamdebashisD <adebashisdas626@gmail.com>
2026-07-04 00:20:34 +0530

    feat: enable URL encoding and integrate auth routes with error handling

--- auth-server/src/app.js
+++ auth-server/src/app.js
@@ -1,16 +1,24 @@
 import express, { urlencoded } from 'express'
+import authRouter from './routes/auth.routes.js'
+import { errorMiddleware } from './middleware/error.middleware.js'
 
 export function createExpressApplication() {
     const app = express()
 
     app.use(express.json())
-    // app.use(urlencoded({ extended: true }))
+    app.use(urlencoded({ extended: true }))
+
+
 
     app.get('/health', (req, res) => {
         return res.status(200).json({
             message: 'Health is OK!✅'
         })
     })
+
+    app.use('/api/auth', authRouter)
+    
+    app.use(errorMiddleware)
 
     return app
 }
