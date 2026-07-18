dfb0433b659160486aadd588042c217f373d588f
IamdebashisD <adebashisdas626@gmail.com>
2026-07-19 00:19:33 +0530

    feat: create App component with routing for Login and Signup pages

---
+++ auth-ui/src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+    </Routes>
+  )
+}
+
+export default App
