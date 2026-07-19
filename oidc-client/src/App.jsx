b2cbb0e109f5bd2b77d62cd16c9ccec75a5187c8
IamdebashisD <adebashisdas626@gmail.com>
2026-07-20 00:42:56 +0530

    feat: implement main App component with routing for Home, Callback, and Dashboard pages

---
+++ oidc-client/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import './App.css'
+import Callback from './pages/Callback'
+import Dashboard from './pages/Dashboard'
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/callback' element={<Callback/>} />
+      <Route path='/dashboard' element={<Dashboard/>} />
+    </Routes>
+  )
+}
+
+export default App
