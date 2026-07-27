646ee528e40e38e37519c1492c3a719c8f853985
IamdebashisD <adebashisdas626@gmail.com>
2026-07-27 23:24:28 +0530

    feat: add Consent route to application for user consent handling

--- auth-ui/src/App.jsx
+++ auth-ui/src/App.jsx
@@ -2,14 +2,16 @@ import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import Consent from './pages/Consent'
 
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login />}/>
+      <Route path='/signup' element={<Signup />}/>
+      <Route path='/consent' element={<Consent />}/>
     </Routes>
   )
 }
