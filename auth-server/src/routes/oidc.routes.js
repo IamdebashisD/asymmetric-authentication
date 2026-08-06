f753c2e834aa0780562ccc00f072dbe511efdf87
IamdebashisD <adebashisdas626@gmail.com>
2026-08-07 00:36:21 +0530

    feat: add revoke endpoint to OIDC routes for token revocation

--- auth-server/src/routes/oidc.routes.js
+++ auth-server/src/routes/oidc.routes.js
@@ -7,5 +7,6 @@ const router = Router()
 router.get('/authorize', authMiddleware, oidcController.authorize)
 router.post('/token', oidcController.token)
 router.get('/userinfo', authMiddleware, oidcController.userInfo)
+router.post("/revoke", oidcController.revoke)
 
 export default router
